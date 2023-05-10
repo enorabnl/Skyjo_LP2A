e783409033ef7b0121cbc8651f85be23e4097f0d
enorabnl <enora.brunel22800@gmail.com>
2023-05-10 13:43:05 +0200

    modif player

--- src/main/java/players/Player.java
+++ src/main/java/players/Player.java
@@ -3,7 +3,7 @@ package players;
 import cards.UV;
 
 public class Player {
-    private Student data;
+    final private Student data;
     private Grid grid;
     private Quotas quotas;
 
@@ -12,22 +12,12 @@ public class Player {
         this.grid=new Grid();
         this.quotas=new Quotas();
     }
-
-    //getters & setters
     public Grid getGrid() {
         return grid;
     }
-    public void setGrid(Grid grid) {
-        this.grid = grid;
-    }
-
     public Student getData() {
         return data;
     }
-    public void setData(Student data) {
-        this.data = data;
-    }
-
     public Quotas getQuotas() {
         return quotas;
     }
@@ -36,28 +26,38 @@ public class Player {
             for(int j=0;j<4;j++){
                 UV card=getGrid().getGrid()[i][j];
                 int credits= card.getCredits();
-                if(card.getClass().getSimpleName().equals("CS")){
-                    if(card.isVisible()){
-                        getQuotas().setCSCredits(getQuotas().getCSCredits()+credits);
+                switch (card.getClass().getSimpleName()) {
+                    case "CS" -> {
+                        if (card.isVisible()) {
+                            getQuotas().setCSCredits(getQuotas().getCSCredits() + credits);
+                        }
                     }
-                } else if (card.getClass().getSimpleName().equals("TM")) {
-                    if(card.isVisible()){
-                        getQuotas().setTMCredits(getQuotas().getTMCredits()+credits);                    }
-                } else if (card.getClass().getSimpleName().equals("EC")) {
-                    if(card.isVisible()){
-                        getQuotas().setECCredits(getQuotas().getECCredits()+credits);                    }
-                }else{
-                    if(card.isVisible()){
-                        getQuotas().setT2SCredits(getQuotas().getT2SCredits()+credits);                    }                }
+                    case "TM" -> {
+                        if (card.isVisible()) {
+                            getQuotas().setTMCredits(getQuotas().getTMCredits() + credits);
+                        }
+                    }
+                    case "EC" -> {
+                        if (card.isVisible()) {
+                            getQuotas().setECCredits(getQuotas().getECCredits() + credits);
+                        }
+                    }
+                    case "T2S" -> {
+                        if (card.isVisible()) {
+                            getQuotas().setT2SCredits(getQuotas().getT2SCredits() + credits);
+                        }
+                    }
+                    default -> {
+                    }
+                }
             }
         }
     }
     public String toString() {
-        return data.toString()+"\n"+getQuotas().toString();
+        return data+"\n"+getQuotas().toString();
     }
     public void displayHand(){
-        System.out.println("\nPlayer : "+toString());
+        System.out.println("\nPlayer : "+ this);
         getGrid().displayGrid();
     }
-
 }
