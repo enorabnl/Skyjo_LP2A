f6444165cde5c48251a58a9bfc5115f733a51b65
enorabnl <enora.brunel22800@gmail.com>
2023-05-12 09:53:52 +0200

    private

--- src/main/java/skyjo/Game.java
+++ src/main/java/skyjo/Game.java
@@ -46,7 +46,7 @@ public class Game {
             drawInDiscard();
         }
     }
-    public void drawInDeck(){
+    private void drawInDeck(){
         Scanner scanner=new Scanner(System.in);
         int choice;
         UV drawnCard=deck.drawACard();
@@ -63,17 +63,17 @@ public class Game {
             replaceACard(drawnCard);
         }
     }
-    public void drawInDiscard(){
+    private void drawInDiscard(){
         UV drawnCard=discard.drawACard();
         drawnCard.makeVisible();
         System.out.println("You drawn : " + drawnCard);
         replaceACard(drawnCard);
     }
-    public void replaceACard( UV card){
+    private void replaceACard( UV card){
         System.out.println("You have to replace a card of your grid.");
         discard.addCard(currentPlayer.getGrid().swapCardsGrid(card,chooseACard()));
     }
-    public void makeACardVisible(){
+    private void makeACardVisible(){
         System.out.println("You have to turn over a card of your grid");
         Coordinates coordinate=chooseACard();
         while(currentPlayer.getGrid().getGrid()[coordinate.getLine()][coordinate.getColumn()].isVisible()){
@@ -82,7 +82,7 @@ public class Game {
         }
         currentPlayer.getGrid().getGrid()[coordinate.getLine()][coordinate.getColumn()].makeVisible();
     }
-    public Coordinates chooseACard(){
+    private Coordinates chooseACard(){
         Scanner scanner=new Scanner(System.in);
         int line;
         int column;
@@ -96,7 +96,50 @@ public class Game {
         }while(column<1 || column>4);
         return new Coordinates(line-1,column-1);
     }
-    public ArrayList<Player> findWinners(){
+
+    /*
+    The DISPLAY methods
+     */
+    private void displayHands(){
+        for (Player p:listOfPlayers) {
+            p.displayHand();
+            System.out.println("\n");
+        }
+    }
+    private void displayEnd(){
+        System.out.println("----------The game is done !----------\n");
+        for (Player p:listOfPlayers) {
+            p.getGrid().makeTheGridVisible();
+            p.displayHand();
+        }
+        displayWinners();
+    }
+    private void displayWinners(){
+        if(findWinners().size()>1){
+            System.out.println("The winners are : ");
+        }else{
+            System.out.println("The winner is : ");
+        }
+        for (Player p:findWinners()) {
+            System.out.println(p.getData().toString());
+        }
+    }
+
+    /*
+    The TEST and SETUP methods
+     */
+    private void distributeCards(){
+        for(Player p: listOfPlayers){
+            Grid grid=p.getGrid();
+            for(int line=0;line< grid.getGrid().length;line++){
+                for(int column=0;column<grid.getGrid()[line].length;column++){
+                    grid.getGrid()[line][column]=deck.drawACard();
+                }
+            }
+        }
+        discard.addCard(deck.drawACard());
+    }
+    private ArrayList<Player> findWinners(){
         ArrayList<Player> listOfWinner=new ArrayList<>();
         listOfWinner.add(listOfPlayers.get(0));
         int totalWinner=listOfWinner.get(0).getQuotas().getTotal();
@@ -112,63 +155,26 @@ public class Game {
         }
         return listOfWinner;
     }
-    /*
-    The DISPLAY methods
-     */
-
-    public void displayHands(){
-        for (Player p:listOfPlayers) {
-            p.displayHand();
-            System.out.println("\n");
-        }
-    }
-    public void displayEnd(){
-        System.out.println("----------The game is done !----------\n");
-        for (Player p:listOfPlayers) {
-            p.getGrid().makeTheGridVisible();
-            p.displayHand();
-        }
-        displayWinners();
-    }
-    public void displayWinners(){
-        if(findWinners().size()>1){
-            System.out.println("The winners are : ");
-        }else{
-            System.out.println("The winner is : ");
-        }
-        for (Player p:findWinners()) {
-            System.out.println(p.getData().toString());
-        }
-    }
-    public void distributeCards(){
-        for(Player p: listOfPlayers){
-            Grid grid=p.getGrid();
-            for(int line=0;line< grid.getGrid().length;line++){
-                for(int column=0;column<grid.getGrid()[line].length;column++){
-                    grid.getGrid()[line][column]=deck.drawACard();
-                }
-            }
-        }
-        discard.addCard(deck.drawACard());
-    }
-    /*
-    The TEST and SETUP methods
-     */
-    public void setup(){
-        Scanner scanner = new Scanner(System.in);
-        int playAgain=3;
+    private boolean noGridVisible(){
+        boolean visible;
+        int i=0;
         do{
-            if(playAgain==3){
-                newPlayers();
-            }
-            play();
-            do{
-                System.out.println("Are you done playing ? (Enter 1)\nDo you want play again with the same players ? (Enter 2)\nDo you want to play again, btu with different players ? (Enter 3)\n");
-                playAgain=scanner.nextInt();
-            }while(playAgain<1 || playAgain>3);
-        }while(playAgain!=1);
+            Grid grid=listOfPlayers.get(i).getGrid();
+            visible=grid.isGridVisible();
+            i++;
+        }while (!visible && i<listOfPlayers.size());
+        return !visible;
     }
-    public void newPlayers(){
+    private void putCardsBackToDeck(){
+        if(!discard.getListOfCard().isEmpty()){
+            for (UV card:discard.getListOfCard()) {
+                card.hide();
+                deck.getListOfCard().add(card);
+                discard.getListOfCard().remove(card);
+            }
+        }
+    }
+    private void newPlayers(){
         Scanner scanner = new Scanner(System.in);
         do{
             System.out.println("How many players ? (2 to 8 players)");
@@ -179,6 +185,8 @@ public class Game {
             listOfPlayers.add(new Player(scanner.next()));
         }
     }
+
+
     public void play(){
         putCardsBackToDeck();
         deck.mixPile();
@@ -202,24 +210,18 @@ public class Game {
         }while(noGridVisible());
         displayEnd();
     }
-    public void putCardsBackToDeck(){
-        if(!discard.getListOfCard().isEmpty()){
-            for (UV card:discard.getListOfCard()) {
-                card.hide();
-                deck.getListOfCard().add(card);
-                discard.getListOfCard().remove(card);
-            }
-        }
-
-    }
-    public boolean noGridVisible(){
-        boolean visible;
-        int i=0;
+    public void setup(){
+        Scanner scanner = new Scanner(System.in);
+        int playAgain=3;
         do{
-            Grid grid=listOfPlayers.get(i).getGrid();
-            visible=grid.isGridVisible();
-            i++;
-        }while (!visible && i<listOfPlayers.size());
-        return !visible;
+            if(playAgain==3){
+                newPlayers();
+            }
+            play();
+            do{
+                System.out.println("Are you done playing ? (Enter 1)\nDo you want play again with the same players ? (Enter 2)\nDo you want to play again, btu with different players ? (Enter 3)\n");
+                playAgain=scanner.nextInt();
+            }while(playAgain<1 || playAgain>3);
+        }while(playAgain!=1);
     }
 }
